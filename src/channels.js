0258a2e75e9ac877984d42644376017dc0e85d16
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2024-04-11 13:19:20 +0700

    commit

--- src/channels.js
+++ src/channels.js
@@ -62,7 +62,7 @@ module.exports = function (app) {
   // });
 
   app.service("messages").publish("created", (data, conn) => {
-    console.log("tesssss", data.dataValues);
+    console.log("tessss", data.dataValues);
     return app.channel(data.dataValues);
   });
 
