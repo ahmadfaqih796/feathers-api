a424d70770e49e90f3451456e2ac3e1d70e5221e
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2024-06-23 11:00:37 +0700

    message service

---
+++ src/models/messages.model.js
@@ -0,0 +1,61 @@
+// See https://sequelize.org/master/manual/model-basics.html
+// for more of what you can do here.
+const Sequelize = require("sequelize");
+const DataTypes = Sequelize.DataTypes;
+
+module.exports = function (app) {
+  const sequelizeClient = app.get("sequelizeClient");
+  const messages = sequelizeClient.define(
+    "messages",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      client_id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+      },
+      user_id: {
+        allowNull: false,
+        type: DataTypes.UUID,
+      },
+      comment_id: {
+        type: DataTypes.UUID,
+      },
+      comment: {
+        type: DataTypes.TEXT,
+      },
+      created_by: {
+        type: DataTypes.STRING(100),
+      },
+    },
+    {
+      hooks: {
+        beforeCount(options) {
+          options.raw = true;
+        },
+      },
+    }
+  );
+
+  // eslint-disable-next-line no-unused-vars
+  messages.associate = function (models) {
+    // Define associations here
+    // See https://sequelize.org/master/manual/assocs.html
+
+    messages.belongsTo(models.users, {
+      forignKey: "user_id",
+      as: "user",
+    });
+    // messages.hasMany(models.comments, {
+    //   forignKey: 'comment_id',
+    //   targetKey:'comment_id',
+    //   as: 'children',
+    // });
+  };
+
+  return messages;
+};
--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,11 @@
 const users = require('./users/users.service.js');
 const address = require('./address/address.service.js');
 const product = require('./product/product.service.js');
+const messages = require('./messages/messages.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(users);
   app.configure(address);
   app.configure(product);
+  app.configure(messages);
 };
