ce1b1ffa11b5c6e899827a90516e31fcd5617f6a
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2024-06-25 21:23:59 +0700

    rooms service

--- src/services/index.js
+++ src/services/index.js
@@ -2,10 +2,12 @@ const users = require('./users/users.service.js');
 const address = require('./address/address.service.js');
 const product = require('./product/product.service.js');
 const messages = require('./messages/messages.service.js');
+const rooms = require('./rooms/rooms.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(users);
   app.configure(address);
   app.configure(product);
   app.configure(messages);
+  app.configure(rooms);
 };
